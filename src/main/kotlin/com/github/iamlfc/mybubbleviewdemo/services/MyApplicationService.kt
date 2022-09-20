7cb06d1c7d64c92e77f4e4e5f206a274ac650250
GitHub Action <action@github.com>
2022-09-20 10:38:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/iamlfc/mybubbleviewdemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/iamlfc/mybubbleviewdemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.iamlfc.mybubbleviewdemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iamlfc.mybubbleviewdemo.MyBundle
 
 class MyApplicationService {
 
